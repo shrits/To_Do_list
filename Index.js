21d691d85def7b5941b545a874ac3f2b33ce0076
shrits <santrashritesh2003@gmail.com>
2024-09-12 18:32:06 +0530

    First Commit

---
+++ Index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const PORT = 2000;
+
+//init app
+
+const app = express();
+
+// view engine
+
+app.set('view engine', 'ejs');
+
+// listen server
+app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
