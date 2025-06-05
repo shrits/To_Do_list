8f5bdb4b311c46c62b4a07a944ad6d012758ef5d
shrits <santrashritesh2003@gmail.com>
2025-06-05 20:21:43 +0530

    committed

--- Index.js
+++ Index.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const PORT = process.env.PORT|| 2000;
+const PORT = process.env.PORT|| 3000;
 
 //init app
 
